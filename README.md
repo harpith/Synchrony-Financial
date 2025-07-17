79a45fae3bdc304be93198912b5a28419bdc9985
harpith <harpithrao2004@gmail.com>
2025-07-17 20:35:21 +0530

    1

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Synchrony-Financial
 
 ## hello harpith Bhai
+
+#cd Synchrony-Financial
+# npm install
+# npm run dev
---
+++ Synchrony-Financial/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+ return(
+  <>
+  <div>Hello World!</div>
+  <button type="button" class="btn btn-primary">Primary</button>
+  </>
+ )
+}
+
+export default App
